937fddbf3c003cb770ebec849dba7b6ddb6eab5d
timothy-r <tim.rodger@gmail.com>
2013-06-29 12:42:54 +0100

    first commit of boiler plate files

---
+++ app/controllers/PhotoController.php
@@ -0,0 +1,79 @@
+<?php
+
+class PhotoController extends \BaseController {
+
+	/**
+	 * Display a listing of photos
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+        return 'A list of photos';
+	}
+
+	/**
+	 * Show the form for creating a new photo
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		//
+	}
+
+	/**
+	 * Store a newly created photo in storage
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+	/**
+	 * Display the specified photo
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified photo
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified photo in storage
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified photo from storage
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+}
