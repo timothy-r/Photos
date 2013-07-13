8ca3ffdd29a62c8bff2c5d04432a056ae0e7813f
timothy-r <tim.rodger@gmail.com>
2013-07-13 16:30:15 +0100

    implemented helper function

--- app/tests/FunctionsTest.php
+++ app/tests/FunctionsTest.php
@@ -5,8 +5,8 @@ class FunctionsTest extends TestCase
 
     public function testLinkTo()
     {
-        $actual = link_tag('dogs/1', 'Show dog');
-        $expected = "<a href='http://:/dogs/1'>Show dog</a>";
+        $actual = link_tag('/dogs/1', 'Show dog');
+        $expected = '<a href="/dogs/1">Show dog</a>';
         $this->assertSame($expected, $actual);
     }
 }
