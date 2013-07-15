3654a5a711bd526f770021617bee7009efe1d777
Tim Rodger <tim.rodger@gmail.com>
2013-07-15 23:45:16 +0100

    removed unused controller updated environment config

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -28,7 +28,8 @@ $app->redirectIfTrailingSlash();
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('your-machine-name'),
+	'local' => array('photos.ace.com'),
+	'production' => array('photos.timjr.net'),
 
 ));
 
