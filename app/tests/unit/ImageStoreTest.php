74f8fd3072c52bb8c7cff23d2e5cdf824ed9ef97
Tim Rodger <tim.rodger@gmail.com>
2013-07-30 22:19:54 +0100

    Adds a test for ImageStore::get()

--- app/tests/unit/ImageStoreTest.php
+++ app/tests/unit/ImageStoreTest.php
@@ -5,18 +5,29 @@ use Ace\Photos\Image;
 class ImageStoreTest extends \PHPUnit_Framework_TestCase
 {
 
+    protected $image_store;
+
+    public function setUp()
+    {
+        $this->image_store = new ImageStore;
+    }
+
     public function testCanAddAnImage()
     {
         $image = new Image;
-        $image_store = new ImageStore;
-        $result = $image_store->add($image);
+        $result = $this->image_store->add($image);
         $this->assertSame(true, $result);
     }
 
     public function testCanListImages()
     {
-        $image_store = new ImageStore;
-        $images = $image_store->all();
+        $images = $this->image_store->all();
         $this->assertTrue(is_array($images));
+    }
+
+    public function testCanGetImage()
+    {
+        $image = $this->image_store->get();
+        #$this->assertTrue(is_array($images));
     }
 }
