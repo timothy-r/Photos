c985cefdefc76e8bbe056e9e0abb8637c74b8587
Tim Rodger <tim.rodger@gmail.com>
2013-07-31 23:42:08 +0100

    Adds missing Photo view

---
+++ app/views/photo.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Cool Photo</title>
+</head>
+<body>
+<h1>Photo</h1>
+<div><?php echo $photo->getName(); ?></div>
+
+<a href="/photos/create">Add</a>
+
+</body>
+</html>
+
