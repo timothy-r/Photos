ec19259fe1f6d606ee3a5318191be7be4a28856a
Tim Rodger <tim.rodger@gmail.com>
2013-08-10 18:18:59 +0100

    Adds debugger config

---
+++ app/config/packages/annotatecms/debugger/debugger.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+
+    "enabled"      => TRUE,
+    "mode"         => NULL,
+    "logDirectory" => storage_path() . DIRECTORY_SEPARATOR . "logs",
+    "email"        => NULL,
+    "panels"       => array(
+        "routing",
+    ),
+);
