9068eaaa813f7a1eff2f997cb9c6d90b169c8ac7
Tim Rodger <tim.rodger@gmail.com>
2013-09-07 17:39:25 +0100

    Adds an image element to create photo form

--- app/views/photos-create.php
+++ app/views/photos-create.php
@@ -8,6 +8,7 @@
 <h1>Add a photo</h1>
 <form method="POST" action="/photos">
 <input name="name" type="text" placeholder="Name:"></input>
+<input name="image" type="file" placeholder="Pick an image"></input>
 <input name="submit" type="submit" value="Add"></input>
 </form>
 <a href="/photos">List</a>
