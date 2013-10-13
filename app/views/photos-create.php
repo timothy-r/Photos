c9a81b575bdf2b1427c26acebd95aa81a8f32da0
Tim Rodger <tim.rodger@gmail.com>
2013-10-13 15:21:17 +0100

    Adds a file select box to add image form

--- app/views/photos-create.php
+++ app/views/photos-create.php
@@ -8,6 +8,9 @@
 <h1>Add a photo</h1>
 <form method="POST" action="/photos">
 <input name="name" type="text" placeholder="Name:"></input>
+<br/>
+<input name="file" type="file" placeholder="Photo:"></input>
+<br/>
 <input name="submit" type="submit" value="Add"></input>
 </form>
 <a href="/photos">List</a>
